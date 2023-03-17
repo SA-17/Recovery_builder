b087e3f9649bd1d9e464dabdd674ccd89d208b49
bread-up <breadup096@gmail.com>
2023-03-17 12:06:37 +0100

    minor update
    Signed-off-by: bread-up <breadup096@gmail.com>

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ ok first before blaming my script if you got a bootloop or the build is unsucces
 
 1) froke
 
-2) go to .github/workflows/recovery.yml
+2) edit config.sh
 
 3) read comments and edit required values to your own
 
-4) enable actions if isnt enabled
+4) enable actions if isnt enabled with write permission to perform (save the build)
 
 5) run
--- config.sh
+++ config.sh
@@ -1,14 +1,18 @@
 #!/bin/bash env
 
-RTYPE="twrp"
-MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git"
-BRANCH="twrp-9.0"
-DEVICE="m10lte"
-DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte"
-DT_BR="twrp"
-MANUFACTURER="samsung"
-TARGET="recovery"
+# about the recovery
+RTYPE="twrp" # supported: pbrp, shrp, twrp
+MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
+BRANCH="twrp-9.0" # the branch of manifest
+
+# about your device
+DEVICE="m10lte" # codename used in device tree
+DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
+DT_BR="twrp" # device tree branch
+MANUFACTURER="samsung" # device manufacturer or vendor
+TARGET="recovery" # build method: recoveryimage, bootimage, vendorboot
+OT="omni" # recovery type: omni, twrp, something else
+
+# you dont need to edit these
 BTYPE="eng"
-OT="omni"
-IMAGE="image"
 SYM="_"
