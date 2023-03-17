9b51e03a4796aeb2ed0265304d04899a9ec87a46
bread-up <breadup096@gmail.com>
2023-03-17 11:52:37 +0100

    remake the srcipt
    Signed-off-by: bread-up <breadup096@gmail.com>

---
+++ config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash env
+
+RTYPE="twrp"
+MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git"
+BRANCH="twrp-9.0"
+DEVICE="m10lte"
+DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte"
+DT_BR="twrp"
+MANUFACTURER="samsung"
+TARGET="recovery"
+BTYPE="eng"
+OT="omni"
+IMAGE="image"
+SYM="_"
