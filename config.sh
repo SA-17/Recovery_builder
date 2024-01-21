795f54ef6af27918a8e206074f6f9d045c3f397f
Draken-tokyo-007 <152719209+Draken-tokyo-007@users.noreply.github.com>
2024-01-21 16:08:02 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash env
 
 # about the custom recovery
-export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+export NAME="shrp" # supported: pbrp, shrp, twrp
+export MANIFEST="https://github.com/SHRP/manifest.git" # the link of manifest
+export BRANCH="SHRPv3_11.0" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
+export DEVICE="sky" # codename used in device tree
+export DT_LINK="https://github.com/sm4450-development/device_xiaomi_sky.git" # device tree link
 export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export VENDOR="xiaomi" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
-export TYPE="omni" # recovery type: omni, twrp, something else
+export TYPE="shrp" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
 export SYM="_"
